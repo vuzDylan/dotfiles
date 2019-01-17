ef6385464edff39d1c07de61fe7c241403e34fbc
Dylan Vuz <vuzdylan@gmail.com>
2019-01-16 18:56:15 -0800

    Update
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- setup.sh
+++ setup.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 
-command -v ag >/dev/null 2>&1 || { echo "I require ag but it's not installed.  Aborting." >&2; exit 1; }
 command -v git >/dev/null 2>&1 || { echo "I require git but it's not installed.  Aborting." >&2; exit 1; }
 command -v zsh >/dev/null 2>&1 || { echo "I require zsh but it's not installed.  Aborting." >&2; exit 1; }
 command -v tmux >/dev/null 2>&1 || { echo "I require tmux but it's not installed.  Aborting." >&2; exit 1; }
 command -v nvim >/dev/null 2>&1 || { echo "I require nvim but it's not installed.  Aborting." >&2; exit 1; }
+command -v pyenv >/dev/null 2>&1 || { echo "I require pyenv but it's not installed.  Aborting." >&2; exit 1; }
 
 
-git submodule init
-git submodule update
+pyenv virtualenv --version gcc-5-glibc-2.23/3.6.3rc1+ neovim;
+source /home/vuzdylan/virtualenvs/neovim/bin/activate;
 
-dotfile_location=`pwd`
+
+git submodule init;
+git submodule update;
+
+dotfile_location=$(pwd)
 
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
-ln -s $dotfile_location/aliases $HOME/.aliases
-ln -s $dotfile_location/functions $HOME/.functions
-ln -s $dotfile_location/oh-my-zsh $HOME/.oh-my-zsh
-ln -s $dotfile_location/zshrc $HOME/.zshrc
-ln -s $dotfile_location/tmux.conf $HOME/.tmux.conf
-ln -s $dotfile_location/nvim $HOME/.config/nvim
-ln -s $dotfile_location/dircolors $HOME/.dircolors
-ln -s $dotfile_location/gitconfig $HOME/.gitconfig
+ln -s "$dotfile_location/aliases" "$HOME/.aliases"
+ln -s "$dotfile_location/functions" "$HOME/.functions"
+ln -s "$dotfile_location/oh-my-zsh" "$HOME/.oh-my-zsh"
+ln -s "$dotfile_location/zshrc" "$HOME/.zshrc"
+ln -s "$dotfile_location/tmux.conf" "$HOME/.tmux.conf"
+ln -s "$dotfile_location/nvim" "$HOME/.config/nvim"
+ln -s "$dotfile_location/dircolors" "$HOME/.dircolors"
+ln -s "$dotfile_location/gitconfig" "$HOME/.gitconfig"
